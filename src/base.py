a4613ed0db6bf7a851c99cdd910937affbc6e4cd
Hardik Sharma <hardiksharmah73@gmail.com>
2024-02-18 16:16:34 +0530

    virtual env hanges

---
+++ src/base.py
@@ -0,0 +1,26 @@
+class Color :
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
+
+class Sentence :
+    def __init__(self, information : dict):
+        self.id = information.get("sent_id", None)
+        self.partition = self.id.split(".")[-1]
+        self.original = information.get("text", "")
+        self.POStags = information.get("POS", [])
+
+    def __str__(self):
+        text = f"{Color.YELLOW}Sentence ID : {self.id}{Color.END}\n"
+        for token,POStag in zip(self.original.split(" "), self.POStags):
+            text += f"{Color.CYAN}{token:<12.10} {Color.BOLD}{Color.GREEN}{POStag:<8.4}{Color.END} \n"
+
+        return text
---
+++ src/config.py
@@ -0,0 +1,3 @@
+class Config :
+    modelSavePath = "../checkpoints/"
+    dataPath = "../datasets/"
